5b8fce3714bbd266a4e9d89a101bb26245cef331
Steve Konves <stephen.konves@gmail.com>
2021-04-25 20:08:59 -0700

    feat: improve CLI

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 [![master](https://github.com/skonves/graphql-server-types/workflows/build/badge.svg?branch=master&event=push)](https://github.com/skonves/graphql-server-types/actions?query=workflow%3Abuild+branch%3Amaster+event%3Apush)
+[![master](https://img.shields.io/npm/v/graphql-server-types)](https://www.npmjs.com/package/graphql-server-types)
 
 # graphql-server-types
 
@@ -8,19 +9,75 @@ Generate intergalactic-quality server-side Typescript types from GraphQL schemas
 
 The generator is available using the `generate-server-types` command.
 
-You can pipe a schema the generator:
+### Input file
+
+Use `-i`, `--input` to supply the input file path:
+
+```
+generate-server-types --input path/to/your/schema.graphql
+```
+
+Or pipe contents in via stdio:
 
 ```
 cat path/to/your/schema.graphql | generate-server-types
 ```
 
-Or provide a `--schema` option:
+### Output file
+
+Use `-o`, `--output` to supply the output file path:
 
 ```
-generate-server-types --schema path/to/your/schema.graphql
+generate-server-types --output path/to/your/types.d.ts
 ```
 
-Note that output will be formatted with [Prettier](https://prettier.io/). If a local Prettier config files exists local, those settings will be used; otherwise, the default formatting rules will be used.
+If an output file path is not supplied, then the output is written to stdout:
+
+```
+generate-server-types > path/to/your/types.d.ts
+```
+
+### Formatting
+
+The generated output will be formatted with [Prettier](https://prettier.io/). The path to a config file may be supplied; otherwise, the local config or default settings will be used.
+
+Use `-p`, `--prettierConfig` specify the file path to your Prettier config:
+
+```
+generate-server-types --prettierConfig path/to/your/.prettierrc
+```
+
+### Context and Info
+
+The generated output will import `Context` and `Info` types from a sibling `./context.ts` file. These allow you to specify the type of context and info objects that the GraphQL server will pass to your resolvers. This generator _does not_ generate the file for you; however, it is required. At minimum, create a file named `context.ts` next to your generated types file with the following content.
+
+Use `-c`, `--contextLocation` override the import path to your context types file:
+
+```
+generate-server-types --contextLocation ./import/path/to/your/context.ts
+```
+
+### Custom Scalars
+
+The generated output will import types for custom scalars from a sibling `./scalars.ts` file. This generator _does not_ generate the file for you; however, it is required. At minimum, create a file named `scalars.ts` next to your generated types file and export (or re-export) a type for each custom scalar.
+
+```ts
+export type MyCustomScalar = string;
+```
+
+Use `-s`, `--scalarsLocation` override the import path to your custom scalar types file:
+
+```
+generate-server-types --scalarsLocation ./import/path/to/your/scalars.ts
+```
+
+### Watch mode
+
+Use `-w`, `--watch` to run in watch mode. In watch mode, both `--input` and `--output` must be specified (you can't leverage stdio/stdout). Running in watch mode will immediately generate output file and then update the output file on each subsequent change to the input file.
+
+```
+generate-server-types --input schema.graphql --output types.g.ts --watch
+```
 
 ## Programatic Usage
 
@@ -35,48 +92,17 @@ const schema = readFileSync('path/to/your/schema.graphql').toString('utf8');
 const serverTypes = generateServerTypes(schema);
 ```
 
-The second `options` parameter allows for custom prettier config and other tweaks.
+The second `options` parameter allows for custom formatting options and other tweaks.
 
 ```js
 const serverTypes = generateServerTypes(schema, {
   prettierOptions: { singleQuote: true },
+  contextLocation: './import/path/to/your/context.ts',
+  scalarsLocation: './import/path/to/your/scalars.ts',
 });
 ```
 
-## Imported types
-
-### Context and Info
-
-The generated output will import `Context` and `Info` types from a sibling `./context.ts` file. These allow you to specify the type of context and info objects that the GraphQL server will pass to your resolvers. This generator _does not_ generate the file for you; however, it is required. At minimum, create a file named `context.ts` next to your generated types file with the following content.
-
-```ts
-export type Context = any;
-export type Info = any;
-```
-
-It is possible to override the location from which the context types are imported by using the `contextLocation` option.
-
-```js
-const serverTypes = generateServerTypes(schema, {
-  contextLocation: '../types.ts',
-});
-```
-
-### Custom Scalars
-
-The generated output will import types for custom scalars from a sibling `./scalars.ts` file. This generator _does not_ generate the file for you; however, it is required. At minimum, create a file named `scalars.ts` next to your generated types file and export (or re-export) a type for each custom scalar.
-
-```ts
-export type MyCustomScalar = string;
-```
-
-It is possible to override the location from which the scalar types are imported by using the `scalarsLocation` option.
-
-```js
-const serverTypes = generateServerTypes(schema, {
-  scalarsLocation: '../types.ts',
-});
-```
+Watch mode is not available during programmatic usage.
 
 ## For Contributors:
 
--- src/cli.ts
+++ src/cli.ts
@@ -1,42 +1,146 @@
 #!/usr/bin/env node
 
-import { readFile } from 'fs/promises';
+import { readFile, writeFile } from 'fs/promises';
+import { watchFile } from 'fs';
 
 import * as chalk from 'chalk';
-import { resolveConfigFile, Options as PrettierOptions } from 'prettier';
+import { resolveConfigFile, Options as PrettierOptions, util } from 'prettier';
 import yargs = require('yargs/yargs');
 import { hideBin } from 'yargs/helpers';
 
-import { generateServerTypes } from './index';
+import { generateServerTypes, Options } from './index';
 
-const { argv } = yargs(hideBin(process.argv));
+const { argv } = yargs(hideBin(process.argv))
+  .option('input', {
+    alias: 'i',
+    string: true,
+    description:
+      'Path to a file that contains a GraphQL schema. Reads from stdin if omitted',
+    requiresArg: true,
+  })
+  .option('output', {
+    alias: 'o',
+    string: true,
+    description: 'Path of the output file. Writes to stdout if omitted.',
+    requiresArg: true,
+  })
+  .option('scalarsLocation', {
+    alias: 's',
+    string: true,
+    description: 'Path for custom scalars import. Defaults to "./scalars".',
+    requiresArg: true,
+  })
+  .option('contextLocation', {
+    alias: 'c',
+    string: true,
+    description: 'Path for context import. Defaults to "./context".',
+    requiresArg: true,
+  })
+  .option('prettierConfig', {
+    alias: 'p',
+    string: true,
+    description:
+      'Path to a Prettier config file. If omitted, uses the local config file if one exists.',
+    requiresArg: true,
+  })
+  .option('watch', {
+    alias: 'w',
+    boolean: true,
+    description: 'Recreates the output file each time the input file changes.',
+    nargs: 0,
+    implies: ['input', 'output'],
+  });
 
 (async () => {
-  const schemaFile: string | undefined =
-    argv.s?.toString() || argv.schema?.toString();
+  try {
+    const {
+      input,
+      output,
+      scalarsLocation,
+      contextLocation,
+      prettierConfig,
+      watch,
+    } = argv;
 
-  let schema: string | undefined;
-
-  if (schemaFile) schema = (await readFile(schemaFile)).toString('utf8');
-  else if (!process.stdin.isTTY)
-    schema = await readStreamToString(process.stdin);
-
-  if (schema) {
-    const prettierOptions = await getPrettierOptions();
-    try {
-      process.stdout.write(generateServerTypes(schema, { prettierOptions }));
-    } catch (ex) {
-      error('Cannot generate server types', ex.message);
-      process.exit(1);
+    if (watch && !input && !output) {
+      throw new Error(
+        'Must specify input and output files to run in watch mode.',
+      );
     }
-  } else {
-    error(
-      'Cannot generate server types',
-      'Input Error: No input schema provided',
-      `For more info, see: https://www.npmjs.com/package/${
-        require('../package.json').name
-      }/v/${require('../package.json').version}`,
+
+    let schema: string | undefined;
+
+    if (input) {
+      schema = (await readFile(input)).toString('utf8');
+    } else if (!process.stdin.isTTY) {
+      schema = await readStreamToString(process.stdin);
+    } else {
+      throw new Error('No input file provided and nothing to read from stdin');
+    }
+
+    const prettierOptions = await getPrettierOptions(prettierConfig);
+
+    const options: Options = {
+      prettierOptions,
+      contextLocation,
+      scalarsLocation,
+    };
+
+    const serverTypes = tryFn(
+      () => generateServerTypes(schema, options),
+      (ex) => `Error generating server types\n${ex.message}`,
     );
+
+    if (output) {
+      await tryPromise(
+        writeFile(output, serverTypes),
+        (ex) => `Cannot write output file\n${ex.message}`,
+      );
+
+      if (watch) {
+        console.log(
+          chalk.greenBright(
+            `Generated initial types in ${output}. Waiting for changes...`,
+          ),
+        );
+        watchFile(input, async () => {
+          console.log(chalk.greenBright(`Detected change of ${input}`));
+          schema = (await readFile(input)).toString('utf8');
+          let result: string = undefined;
+
+          try {
+            result = generateServerTypes(schema, options);
+          } catch (ex) {
+            console.error(
+              chalk.yellow(`Error generating server types (${ex.message})`),
+            );
+          }
+
+          if (result) {
+            try {
+              await writeFile(output, result);
+              console.log(
+                chalk.greenBright(`Updated ${output}. Waiting for changes...`),
+              );
+            } catch (ex) {
+              console.error(
+                chalk.yellow(`Error writing output file (${ex.message})`),
+              );
+            }
+          } else {
+            console.error(
+              chalk.yellow(
+                `Output file ${output} has not been modified. Waiting for changes...`,
+              ),
+            );
+          }
+        });
+      }
+    } else {
+      process.stdout.write(serverTypes);
+    }
+  } catch (ex) {
+    error('Did not generate server types', '', ex.message);
     process.exit(1);
   }
 })();
@@ -47,12 +151,22 @@ async function readStreamToString(stream: NodeJS.ReadStream) {
   return Buffer.concat(chunks).toString('utf8');
 }
 
-async function getPrettierOptions(): Promise<PrettierOptions | undefined> {
-  const prettierConfigFile = await resolveConfigFile();
+async function getPrettierOptions(
+  override: string | undefined,
+): Promise<PrettierOptions | undefined> {
+  const prettierConfigFile = override || (await resolveConfigFile());
 
-  return prettierConfigFile
-    ? JSON.parse((await readFile(prettierConfigFile)).toString())
-    : undefined;
+  if (!prettierConfigFile) return undefined;
+
+  const configBuffer = await tryPromise(
+    readFile(prettierConfigFile),
+    (ex) => `Cannot read Prettier config\n${ex.message}`,
+  );
+
+  return tryFn(
+    () => JSON.parse(configBuffer.toString('utf8')),
+    (ex) => `Cannot parse Prettier config\n${ex.message}`,
+  );
 }
 
 function error(...lines: string[]): void {
@@ -61,4 +175,31 @@ function error(...lines: string[]): void {
     console.error(chalk.bold.red(line));
   }
   console.error();
+}
+
+function tryFn<T>(fn: () => T, message: string | ((ex: Error) => string)): T {
+  try {
+    return fn();
+  } catch (ex) {
+    if (typeof message === 'function') {
+      throw new Error(`Error: ${message(ex)}`);
+    } else {
+      throw new Error(`Error: ${message}`);
+    }
+  }
+}
+
+async function tryPromise<T>(
+  promise: Promise<T>,
+  message: string | ((ex: Error) => string),
+): Promise<T> {
+  try {
+    return await promise;
+  } catch (ex) {
+    if (typeof message === 'function') {
+      throw new Error(`Error: ${message(ex)}`);
+    } else {
+      throw new Error(`Error: ${message}`);
+    }
+  }
 }
